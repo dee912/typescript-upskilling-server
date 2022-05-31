b8c6eee6c9f3eeb9492008a757b8829271cbc868
Devante <devantespaine@gmail.com>
2022-05-31 15:01:54 +0100

    finish TS setup

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('Express + TypeScript Server is running');
 });
 
 app.listen(port, () => {
